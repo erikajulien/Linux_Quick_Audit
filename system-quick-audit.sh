3766e370cf113019354787ba28c8532c316a048c
Erika Julien <58639511+erikajulien@users.noreply.github.com>
2021-07-04 16:06:19 -0700

    Update system-quick-audit.sh

--- system-quick-audit.sh
+++ system-quick-audit.sh
@@ -1,31 +1,67 @@
 #!/bin/bash
+if [ $UID -ne 0 ]; then
+  echo "Please run this script as root/sudo."
+exit
+fi
 
+#Variables
 var=`date +"%FORMAT_STRING"`
 now=`date +"%m_%d_%Y"`
+exes=$(sudo find /home -type f -perm 777 2>/dev/null)
+output=quick-audit-results
 
-echo "Quick System Audit" >> quick-audit-results.txt
-date >> quick-audit-results.txt
-echo "" >> quick-audit-results.txt
-echo "Machine Type Info:" >> quick-audit-results.txt
-echo $MACHTYPE >> quick-audit-results.txt
-echo -e "Uname info: $(uname -a) \n" >> quick-audit-results.txt
-echo -e "IP Info: $(ip addr | grep inet | tail -2 | head -1) \n" >> quick-audit-results.txt
-echo -e "Hostname: $(hostname -s) \n" >> quick-audit-results.txt
-echo "DNS Servers: " >> quick-audit-results.txt
-cat /etc/resolv.conf | grep nameservers >> quick-audit-results.txt
-echo -e "\nMemory Info:" >> quick-audit-results.txt
-free >> quick-audit-results.txt
-echo -e "\nCPU Info:" >> quick-audit-results.txt
-lscpu | grep CPU >> quick-audit-results.txt
-echo -e "\nDisk Usage:" >> quick-audit-results.txt
-df -h | head -2 >> quick-audit-results.txt
-echo -e "\nWho is logged in: \n $(who -a) \n" >> quick-audit-results.txt
-echo -e "\nSUID Files:" >> quick-audit-results.txt
-find / -type f -perm /4000 >> quick-audit-results.txt
-echo -e "\nTop 10 Processes" >> quick-audit-results.txt
-ps aux -m | awk {'print $1, $2, $3, $4, $11'} | head >> quick-audit-results.txt
-echo "Appending date to file results."
-mv quick-audit-results.txt quick-audit-results-${now}.txt
-echo "Quick audit complete! Created file quick-audit-results-TODAYSDATE.txt."
+#Lists
+files=(
+  '/etc/passwd'
+  '/etc/shadow'
+)
 
+#################SCRIPT START########################
+echo "Quick System Audit" >> $output.txt
+echo $date >> $output.txt
+echo "" >> $output.txt
 
+echo "Machine Type Info:" >> $output.txt
+echo $MACHTYPE >> $output.txt
+echo "" >> $output.txt
+
+echo -e "Uname info: $(uname -a) \n" >> $output.txt
+
+echo -e "Hostname: $(hostname -s) \n" >> $output.txt
+
+echo -e "IP Info: $(ip addr | grep inet | tail -2 | head -1) \n" >> $output.txt
+
+echo "DNS Servers:" >> $output.txt
+cat /etc/resolv.conf | grep nameservers >> $output.txt
+
+echo -e "\nMemory Info:" >> $output.txt
+free >> $output.txt
+
+echo -e "\nCPU Info:" >> $output.txt
+lscpu | grep CPU >> $output.txt
+
+echo -e "\nDisk Usage:" >> $output.txt
+df -h | head -2 >> $output.txt
+
+echo -e "\nWho is logged in:" >> $output.txt
+echo -e "$(who -a) \n" >> $output.txt
+
+echo -e "\nSUID Files:" >> $output.txt
+for exe in $exes; do
+ echo "$exes" >> $output.txt
+done
+
+echo -e "\nTop 10 Processes" >> $output.txt
+ps aux --sort -%mem | awk {'print $1, $2, $3, $4, $11'} | head >> $output.txt
+
+echo -e "\nSensitive /etc file permissions:" >> $output.txt
+for file in ${files[@]}; do
+  ls -l $file >> $output.txt
+done
+
+echo -e "\nEND REPORT" >> $output.txt
+
+echo  "Appending date to file results."
+mv $output.txt $output-${now}.txt
+
+echo "Quick Audit Complete!"
